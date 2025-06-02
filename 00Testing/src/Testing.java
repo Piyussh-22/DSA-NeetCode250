c74ab0f68fa50d0c2c1d7a92fe0d3d433bf86b85
Piyush Raj <piyushraj4734@gmail.com>
2025-06-02 18:17:52 +0530

    init commit

---
+++ 00Testing/src/Testing.java
@@ -0,0 +1,12 @@
+public class Testing {
+    public static void main(String[] args) {
+        int num = 99;
+        System.out.println(num);
+        String numS = Integer.toString(num);
+        System.out.println(numS);
+        int a = numS.charAt(0)-'0';
+        System.out.println(a);
+        System.out.println("hello".length());
+
+    }
+}
