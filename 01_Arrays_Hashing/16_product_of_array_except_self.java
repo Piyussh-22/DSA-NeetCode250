827c2e35331f35345947765c99cf8caaa920c58b
Piyush Raj <piyushraj4734@gmail.com>
2025-06-15 00:51:16 +0530

    init commit

--- 01_Arrays_Hashing/16_product_of_array_except_self.java
+++ 01_Arrays_Hashing/16_product_of_array_except_self.java
@@ -17,6 +17,33 @@ class Solution {
         return ans;
     }
 }
+
+//same logic bit easy to understand
+class Solution {
+    public int[] productExceptSelf(int[] nums) {
+        int n = nums.length;
+        int[] left = new int[n];
+        int[] right = new int[n];
+        int[] ans = new int[n];
+
+        left[0] = 1 ;
+        for(int i = 1 ; i < nums.length ; i++){
+            left[i] = nums[i-1] * left[i-1] ;
+        }
+
+        right[n-1] = 1 ;
+        for(int i = n-2 ; i >=0 ; i-- ){
+            right[i] = right [i+1] * nums [i+1];
+        }
+
+        for(int i = 0 ; i < nums.length ; i++ ){
+            ans[i] = left[i] * right[i];
+        }
+
+        return ans;
+    }
+}
+
 /*238. Product of Array Except Self
 Attempted
 Medium
